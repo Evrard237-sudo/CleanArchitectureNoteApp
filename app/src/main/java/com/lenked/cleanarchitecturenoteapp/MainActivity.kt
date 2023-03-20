c6631b064f13331fa6c1cafb19bf4e626fa5cc65
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2023-03-20 01:05:01 +0100

    first commit

---
+++ app/src/main/java/com/lenked/cleanarchitecturenoteapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.lenked.cleanarchitecturenoteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lenked.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CleanArchitectureNoteAppTheme {
+
+            }
+        }
+    }
+}
